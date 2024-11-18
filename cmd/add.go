24ca8d83a34ce61ccdd2651248e6007fa88a55fe
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-11-18 19:23:08 +0530

    get the last task id

--- cmd/add.go
+++ cmd/add.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -16,14 +18,11 @@ var addCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		_, err := os.Stat(FILE_PATH)
 		isNewFile := os.IsNotExist(err)
+		id := 1
 
-		f, err := os.OpenFile(FILE_PATH, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		f, err := os.OpenFile(FILE_PATH, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 
 		if err != nil {
-			if os.IsExist(err) {
-				fmt.Println("hi")
-			}
-
 			fmt.Println("Failed to open task.csv file")
 			return
 		}
@@ -32,12 +31,37 @@ var addCmd = &cobra.Command{
 		csvWriter := csv.NewWriter(f)
 
 		if isNewFile {
+			id = 1
 			err = csvWriter.Write([]string{"ID", "Description", "CreatedAt", "IsComplete"})
 
 			if err != nil {
 				fmt.Println("Failed to write to task.csv file")
 				return
 			}
+		} else {
+			csvReder := csv.NewReader(f)
+			header, err := csvReder.Read()
+			if err != nil {
+				fmt.Println("Failed to read file")
+				return
+			}
+			fmt.Println(header)
+
+			for {
+				line, err := csvReder.Read()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					fmt.Println("Error reading csv data")
+					return
+				}
+				id, err = strconv.Atoi(line[0])
+				if err != nil {
+					fmt.Println("Failed to parse ID")
+					return
+				}
+			}
+			id++
 		}
 
 		for _, arg := range args {
@@ -45,7 +69,7 @@ var addCmd = &cobra.Command{
 				return
 			}
 
-			err = csvWriter.Write([]string{"1", arg, "time", "false"})
+			err = csvWriter.Write([]string{strconv.Itoa(id), arg, "time", "false"})
 			if err != nil {
 				fmt.Println("Failed to write to task.csv file")
 				return
