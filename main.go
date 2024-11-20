e7ff9962d6de8015d487ce95e6f35d6ea61bcfb6
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-11-20 22:23:24 +0530

    create file package

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "example.com/cmd"
+import (
+	"fmt"
+
+	"example.com/cmd"
+	"example.com/file"
+)
 
 func main() {
+	f, err := file.LoadFile("./task.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	cmd.Execute()
+
+	defer file.CloseFile(f)
 }
