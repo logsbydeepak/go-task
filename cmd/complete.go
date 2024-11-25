01d3ec0b4eb0bf76e252904e1261dca6c34705f1
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-11-25 20:04:07 +0530

    in complete command read all data and store it

--- cmd/complete.go
+++ cmd/complete.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"example.com/file"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +13,45 @@ var completeCmd = &cobra.Command{
 	Use:   "complete",
 	Short: "Mark task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("complete called")
+		isNewFile := file.IsNewFile()
+		var data []string
+
+		csvReader := file.NewReader()
+		header, err := csvReader.Read()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to read file")
+			return
+		}
+		if isNewFile {
+			fmt.Fprintln(os.Stderr, "Not found")
+			return
+		} else {
+
+			err = file.ParseHeader(header)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to parse header")
+				return
+			}
+			data = append(data, header...)
+		}
+
+		for {
+			line, err := csvReader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to read file")
+				return
+			}
+
+			_, err = file.ParseLine(line)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to parse line")
+				return
+			}
+
+			data = append(data, header...)
+		}
 	},
 }
 
