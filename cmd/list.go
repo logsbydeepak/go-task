1e47a485d5a07443255e6b925b1a1b5aa50e61d4
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-11-29 23:10:23 +0530

    refactor list command to use db

--- cmd/list.go
+++ cmd/list.go
@@ -2,13 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"io"
-	"os"
-	"strings"
-	"text/tabwriter"
 
-	"example.com/file"
-	"github.com/mergestat/timediff"
+	"example.com/db"
 	"github.com/spf13/cobra"
 )
 
@@ -16,82 +11,15 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all the task",
 	Run: func(cmd *cobra.Command, args []string) {
-		isNewFile := file.IsNewFile()
-
-		if isNewFile {
-			csvWriter := file.NewWriter()
-			err := file.WriteHeader(csvWriter)
-			if err != nil {
-				fmt.Println("Failed to write header to file")
-				return
-			}
-
-			csvWriter.Flush()
-		}
-
-		showAll, err := cmd.Flags().GetBool("all")
+		tasks, err := db.ListAll()
 		if err != nil {
-			fmt.Println("Failed to parse flag value")
+			fmt.Println("Failed get tasks")
 			return
 		}
 
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.StripEscape)
-
-		csvReader := file.NewReader()
-		header, err := csvReader.Read()
-		if err != nil {
-			fmt.Println("Error reading csv data")
-			return
+		for _, task := range tasks {
+			fmt.Println(task)
 		}
-
-		err = file.ParseHeader(header)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to parse header")
-			return
-		}
-
-		var text strings.Builder
-		for _, each := range header {
-			text.WriteString(each)
-			text.WriteString("\t")
-		}
-		fmt.Fprintln(w, text.String())
-
-	lineLoop:
-		for {
-			line, err := csvReader.Read()
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				fmt.Println("Error reading csv data")
-				return
-			}
-
-			data, err := file.ParseLine(line)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			time := timediff.TimeDiff(data.CreatedAt)
-
-			var text strings.Builder
-			for i, each := range line {
-				if showAll == false && data.IsComplete == true {
-					continue lineLoop
-				}
-
-				if i == file.CREATED_AT {
-					text.WriteString(time)
-				} else {
-					text.WriteString(each)
-				}
-
-				text.WriteString("\t")
-			}
-			fmt.Fprintln(w, text.String())
-		}
-		w.Flush()
 	},
 }
 
