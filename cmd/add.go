3a0258e2b31e273fd96844fd29c631a91cd433ea
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-11-29 23:07:58 +0530

    refactor add command to use db

--- cmd/add.go
+++ cmd/add.go
@@ -1,13 +1,9 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"os"
-	"time"
 
-	"example.com/file"
+	"example.com/db"
 	"github.com/spf13/cobra"
 )
 
@@ -24,44 +20,9 @@ var addCmd = &cobra.Command{
 			return
 		}
 
-		isNewFile := file.IsNewFile()
-		var id int64
-		id = 1
-
-		csvWriter := file.NewWriter()
-		if isNewFile {
-			id = 1
-			err := file.WriteHeader(csvWriter)
-
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Failed to write to file")
-				return
-			}
-		} else {
-			var err error
-			id, err = getID()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				return
-			}
-		}
-
-		task := file.Task{
-			ID:          id,
-			Description: description,
-			CreatedAt:   time.Now(),
-			IsComplete:  false,
-		}
-
-		err := file.WriteTask(csvWriter, task)
+		err := db.Create(description)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to write line into file")
-			return
-		}
-
-		csvWriter.Flush()
-		if err := csvWriter.Error(); err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to write into file")
+			fmt.Println("Failed to create task")
 			return
 		}
 	},
@@ -69,42 +30,4 @@ var addCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-}
-
-func getID() (int64, error) {
-	var id int64 = 1
-	csvReader := file.NewReader()
-	header, err := csvReader.Read()
-
-	if err != nil {
-		return id, errors.New("Failed to read file")
-	}
-
-	err = file.ParseHeader(header)
-	if err != nil {
-		return id, errors.New("Failed to parse header")
-	}
-
-	lineNumber := 1
-	for {
-		line, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return id, errors.New("Failed to read file")
-		}
-
-		data, err := file.ParseLine(line)
-		if err != nil {
-			return id, errors.New("Failed to parse line")
-		}
-
-		id = data.ID
-		lineNumber++
-	}
-	if lineNumber != 1 {
-		id++
-	}
-
-	return id, nil
 }
