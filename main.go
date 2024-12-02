f4e9851e761995ade0799653ab374b395fa85dd0
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-12-02 18:42:01 +0530

    remove csv file

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"example.com/cmd"
 	"example.com/db"
-	"example.com/file"
 )
 
 func main() {
@@ -18,18 +16,10 @@ func main() {
 	defer db.Close()
 
 	err = db.Init()
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	f, err := file.LoadFile("./task.csv")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-
 	cmd.Execute()
-	defer file.CloseFile(f)
 }
